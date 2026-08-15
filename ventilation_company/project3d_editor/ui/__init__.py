ecae4fe06a1590e3d373952088e3e1f1286a9fb3
tolik <example@gmail.com>
2026-08-16 02:55:31 +0300

    програма без 3D редактора

--- ventilation_company/project3d_editor/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""UI-компоненти."""
-
-from ventilation_company.project3d_editor.ui.toolbar import Toolbar
-from ventilation_company.project3d_editor.ui.property_panel import PropertyPanel
-from ventilation_company.project3d_editor.ui.layer_panel import LayerPanel
-from ventilation_company.project3d_editor.ui.tool_settings_panel import ToolSettingsPanel
-
-__all__ = ["Toolbar", "PropertyPanel", "LayerPanel", "ToolSettingsPanel"]
